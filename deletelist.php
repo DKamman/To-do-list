d63a363a8e185a37c1c3a10ccfc4f0da106ca54e
DKamman <42833740+DKamman@users.noreply.github.com>
2020-05-19 13:24:10 +0200

    Updated Delete List query, Fully functional

--- deletelist.php
+++ deletelist.php
@@ -4,17 +4,59 @@
 
 require('inc/dbconn.php');
 
+$sql = 'SELECT * FROM lists WHERE id = :listid';
+$query = $conn->prepare($sql);
+$query->bindParam(':listid', $_GET['listid']);
+$query->execute();
+$result = $query->fetch();
+
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-  $stmt = $conn->prepare('INSERT INTO lists (title) VALUES (:title)');
-
-  $stmt->bindParam(':title', $title);
-  $title =    $_POST['title'];
-
+  $stmt = $conn->prepare('DELETE FROM lists WHERE id = :id');
+  $stmt->bindParam(':id', $id);
+  $id =    $_POST['id'];
   $stmt->execute();
 
   $conn = null;
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Local CSS -->
+    <link rel="stylesheet" href="css/todostyle.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>To do list</title>
+  </head>
+  <body>
+
+    <div class="main-content">
+        <div class="list" style="margin-top: 0; padding-top: 12.5rem">
+            <div class="card" style="min-height: 0;">
+                <div class="card-body">
+                    <h5 class="card-title">Lijst succesvol verwijderd</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
 
 <?php header('Refresh:1; url=index.php'); }else{
 
@@ -41,17 +83,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div class="list" style="margin-top: 0; padding-top: 12.5rem">
             <div class="card" style="min-height: 0;">
                 <div class="card-body">
-                    <h5 class="card-title">Een nieuwe lijst toevoegen</h5>
+                    <h5 class="card-title">Weet u zeker dat u de lijst, "<?php echo $result['title'];?>", wilt verwijderen</h5>
 
                     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
                         <div class="form-group">
 
-                        <label for="title">Titel van de lijst</label>
-                        <input class="form-control" type="text" name="title" required>
+                        <input class="form-control" type="hidden" name="id" value="<?php echo $result['id'];?>">
+                        <p><?php echo $result['id'];?></p>
 
-                        <div style="margin-top:1em;">
-                            <a href="index.php"><button type="button" class="btn btn-danger">Annuleren</button></a>
-                            <button type="submit" class="btn btn-success">Toevoegen</button>
+                        <div style="margin-top: 1em;">
+                          <button type="submit" class="btn btn-danger">Verwijderen</button>
+                          <a href="index.php"><button type="button" class="btn btn-success">Annuleren</button></a>
                         </div>
 
                         </div>
